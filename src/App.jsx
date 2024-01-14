b5f0105c97e27987712204ae20dad6ab71cfae2a
Aleksandr <alekcandr206@gmail.com>
2024-01-14 16:27:43 +0100

    change styles and folders reorder

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header/Header'
+import ReferenceItem from './components/ReferenceItem/ReferenceItem'
 import { referenceList } from './data'
 
-function ReferenceItem(props) {
-  return(
-    <li>
-      <p>
-        {props.title}
-      </p>
-    </li>
-  )
-}
-
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -34,18 +25,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <ul>
-        <ReferenceItem title={referenceList[0].title} />
-      </ul>
+      <section>
+        <ul>
+          <ReferenceItem {... referenceList[0]} />
+        </ul>
+      </section>
     </>
   )
 }
-
-export default App
---
+++ src/components/ReferenceItem/ReferenceItem.jsx
@@ -0,0 +1,9 @@
+export default function ReferenceItem({ title }) {
+    return(
+      <li>
+        <p>
+          {title}
+        </p>
+      </li>
+    )
+  }
