d4203f77f1d9b889ebe5f9720204a3bc3b6db2ad
Aleksandr <alekcandr206@gmail.com>
2024-02-01 23:39:24 +0100

    add modal

--- src/components/ReferenceItem/ReferenceItem.jsx
+++ src/components/ReferenceItem/ReferenceItem.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import Modal from "../Modal/Modal";
+
 export default function ReferenceItem({ link, name }) {
-    return(
-      <li>
-        <a href={link} target="_blank">
-          {name}
-        </a>
-      </li>
-    )
+  const[modal, setModal] = useState(false)
+
+  function openModal() {
+    setModal(true)
   }
+
+  return(
+    <>
+    <li onClick={openModal}>
+      <a href={link} target="_blank">
+        {name}
+      </a>
+    </li>
+
+    <Modal open={modal}>
+      <h3>Hello from modal</h3>
+      <p>Text</p>
+      <button onClick={() => setModal(false)}>close</button>
+    </Modal>
+    </>
+  )
+}
