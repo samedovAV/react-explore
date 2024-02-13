3e7fdadc34c05e4c9fe0398d97c513fdf730f965
Aleksandr <alekcandr206@gmail.com>
2024-02-13 23:20:01 +0100

    add useCallback and fetching

--- src/components/ReferenceItem/ReferenceItem.jsx
+++ src/components/ReferenceItem/ReferenceItem.jsx
@@ -1,26 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import Modal from "../Modal/Modal";
 
 export default function ReferenceItem({ link, name }) {
-  const[modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
 
-  function openModal() {
-    setModal(true)
-  }
+  const fetchUsers = useCallback(async () => {
+    setLoading(true)
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json()
+    setUsers(users)
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [fetchUsers])
 
   return(
-    <>
-    <li onClick={openModal}>
-      <a href={link} target="_blank">
-        {name}
-      </a>
-    </li>
+    <section>
+      <li onClick={() => setModal(true)}>
+        <a href={link} target="_blank">
+          {name}
+        </a>
+      </li>
 
-    <Modal open={modal}>
-      <h3>Hello from modal</h3>
-      <p>Text</p>
-      <button onClick={() => setModal(false)}>close</button>
-    </Modal>
-    </>
+      <Modal open={modal}>
+        <h3>Hello from modal</h3>
+        <p>Text</p>
+        <button onClick={() => setModal(false)}>close</button>
+      </Modal>
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && <ul>
+          {users.map(user => <li key={user.id}>{user.name}</li>)}
+        </ul>}
+    </section>
   )
 }
